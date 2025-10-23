60e50ba54df3a58140647d1cbbd4b94eb07d08ca
Joshua Robinson <joshuarobinson@macbookpro.lan>
2025-10-22 21:27:23 -0700

    fix: convert deck.js to CommonJS to fix require() in server.js

--- src/deck.js
+++ src/deck.js
@@ -1,10 +1,10 @@
 // Rook card game deck definition
 
-export const COLORS = ['Black', 'Red', 'Green', 'Yellow'];
-export const NUMBERS = Array.from({length: 14}, (_, i) => i + 1);
-export const ROOK_CARD = { color: 'Special', name: 'Rook' };
+const COLORS = ['Black', 'Red', 'Green', 'Yellow'];
+const NUMBERS = Array.from({length: 14}, (_, i) => i + 1);
+const ROOK_CARD = { color: 'Special', name: 'Rook' };
 
-export function createDeck() {
+function createDeck() {
   const deck = [];
   for (const color of COLORS) {
     for (const number of NUMBERS) {
@@ -15,6 +15,4 @@ export function createDeck() {
   return deck;
 }
 
-// Example usage:
-// const deck = createDeck();
-// console.log(deck);
+module.exports = { createDeck, COLORS, ROOK_CARD };
